589ecde91d8daf4bd9d2a76a0101b83b9acbbcba
nxf68849 <srihari.gajulapalli@nxp.com>
2021-08-04 12:54:39 +0530

    Update license and copyright

--- keymaster/libese_transport/EseTransportUtils.cpp
+++ keymaster/libese_transport/EseTransportUtils.cpp
@@ -1,6 +1,5 @@
 /*
- **
- ** Copyright 2020, NXP.
+ ** Copyright 2020-2021 NXP
  **
  ** Licensed under the Apache License, Version 2.0 (the "License");
  ** you may not use this file except in compliance with the License.
@@ -13,8 +12,6 @@
  ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  ** See the License for the specific language governing permissions and
  ** limitations under the License.
- **
- ** Copyright 2020 NXP
  **
  */
 #include <vector>
